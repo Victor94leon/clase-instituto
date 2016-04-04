339d73764b48303e273f04361ac67bcb9f24db51
Víctor <victorlopez94leon@gmail.com>
2016-04-04 10:58:37 +0200

    Módificada la clase Test para crear nombres aleatorios de tres letras

--- Test.java
+++ Test.java
@@ -8,8 +8,16 @@ public class Test
     public void test1()
     {
         Random rdm = new Random();
+
+        String[] listaLetras={"a","b","c","d","e","f","g","h","i","j","k","l","m","n ",
+                "ñ","o","p","q","r","s","t", "u","v","w","x","y","z"};
+        String nombre = null;
         for(int index = 0; index<5; index++){
-            Alumno alumno = new Alumno("a",rdm.nextInt(100));
+            String letra1 = listaLetras[rdm.nextInt(listaLetras.length)];
+            String letra2 = listaLetras[rdm.nextInt(listaLetras.length)];
+            String letra3 = listaLetras[rdm.nextInt(listaLetras.length)];
+            nombre = letra1 + letra2 + letra3;
+            Alumno alumno = new Alumno(nombre,rdm.nextInt(100));
             alumno.addNota(rdm.nextInt(10));
             alumno.addNota(rdm.nextInt(10));
             alumno.mostrarDatos();
